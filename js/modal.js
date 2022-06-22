f611af2ace73857614891c27710d0e9bb10c8d30
Nexuslolz <a.klyosov@inbox.ru>
2022-06-22 10:55:04 +0500

    feat:add interactive for modal window to callback

---
+++ js/modal.js
@@ -0,0 +1,42 @@
+/////// open modal window
+const openModal = document.querySelector('.content-box__number');
+const modal = document.querySelector('.page-header__form');
+const closeModal = document.querySelector('.form-content__wrapper')
+
+openModal.addEventListener('click', function(){
+    if(!(modal.classList.contains('page-header__form_open'))){
+        modal.classList.add('page-header__form_open')
+    }
+})
+closeModal.addEventListener('click', function() {
+    if(modal.classList.contains('page-header__form_open')){
+        modal.classList.remove('page-header__form_open')
+    }
+})
+
+
+/////send form
+const sendForm = document.querySelector('.form-content__submit');
+const input = document.getElementById("popup-input");
+const checkbox = document.querySelector('.form-content__confirm')
+const validCheck =  document.getElementById("validation-check");
+
+function isValid() {
+    if (!input.checkValidity()) {
+        input.innerHTML = input.validationMessage;
+    } else if (!(checkbox.checked)) {
+        validCheck.innerHTML = 'Нужно ваше согласие с условиями политики конфиденциальности!'
+        return 
+    } else {
+        modal.innerHTML = `<div class = "form-content_send">
+<h2 class = "form-content__header">Спасибо!</h2> 
+<p class="form-content__text"> Ваша заявка отправлена.</p>
+ </div>`
+ setTimeout(function(){
+     modal.classList.remove('page-header__form_open')
+     location.reload()
+ },2000)
+    }
+}
+
+sendForm.addEventListener('click', isValid)
