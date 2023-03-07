d95ed2d6d7c46025e415b78fb1528957d6419eae
Nexuslolz <a.klyosov@inbox.ru>
2023-03-07 18:58:50 +0500

    fix: fix adaptive styles

--- js/features.js
+++ js/features.js
@@ -2,18 +2,19 @@ const featBtn = document.querySelector('.features__btn');
 const featBlock = document.querySelector('.features');
 // const navBtn = document.querySelector('.main-nav__button');
 
-navBtn.addEventListener('click',function(){
-    if(featBtn.classList.contains('features__btn_active')){
+navBtn.addEventListener('click', function () {
+    if (featBtn.classList.contains('features__btn_active')) {
         featBtn.classList.remove('features__btn_active')
         featBlock.classList.remove('features_active')
-}})
-featBtn.addEventListener('click', function() {
-    if (featBtn.classList.contains('features__btn_active')){
+    }
+})
+
+featBtn.addEventListener('click', function () {
+    if (featBtn.classList.contains('features__btn_active')) {
         featBtn.classList.remove('features__btn_active')
         featBlock.classList.remove('features_active')
-    }else{
+    } else {
         featBtn.classList.add('features__btn_active')
         featBlock.classList.add('features_active')
     }
 })
-
